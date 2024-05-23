ab9caa7360c8672322cf7f4e9fea59dce36ea8cf
minato <yanggang.chen7@gmai.com>
2024-05-23 11:41:24 +0900

    v1.0（3类3间隔）

--- plot_type3_PIL.py
+++ plot_type3_PIL.py
@@ -6,10 +6,12 @@ output = "./output_pillow_3"
 os.makedirs(output, exist_ok=True)
 
 
+# 随机生成颜色
 def randomcolor():
     return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 
+# 生成前景色和背景色，确保两者不同
 def fgbgcolor():
     rgb_fg = randomcolor()
     rgb_bg = randomcolor()
@@ -18,45 +20,43 @@ def fgbgcolor():
     return rgb_fg, rgb_bg
 
 
-def plot_pillow(rgb_fg, rgb_bg, size, n, output_dir="./output_pillow_3"):
+# 绘制图案函数
+def plot_pillow(
+    rgb_fg, rgb_bg, size, n, grid_line_factor=8, output_dir="./output_pillow_3"
+):
+    # 创建一个填充前景色的图像
     image = Image.new("RGB", (size, size), rgb_fg)
     draw = ImageDraw.Draw(image)
 
-    stripe_width = size / (
-        n * 4
-    )  # 比较好的效果是1.5~6之间，这里数字越大n应该要越小效果比较好，如4和16
-    # Adjust the divisor to control the width of the grid lines
-    for i in range(n + 1):
-        draw.rectangle(
-            [
-                i * size // n - stripe_width // 2,  # Left
-                0,  # Top
-                i * size // n + stripe_width // 2,  # Right
-                size,  # Bottom
-            ],
-            fill=rgb_bg,
-        )
-        draw.rectangle(
-            [
-                0,  # Left
-                i * size // n - stripe_width // 2,  # Top
-                size,  # Right
-                i * size // n + stripe_width // 2,  # Bottom
-            ],
-            fill=rgb_bg,
-        )
+    # 计算网格线的宽度
+    stripe_width = size // (n * grid_line_factor)
 
+    # 画垂直网格线
+    for i in range(n + 1):
+        left = i * size // n - stripe_width // 2
+        right = i * size // n + stripe_width // 2
+        draw.rectangle([left, 0, right, size], fill=rgb_bg)
+
+    # 画水平网格线
+    for i in range(n + 1):
+        top = i * size // n - stripe_width // 2
+        bottom = i * size // n + stripe_width // 2
+        draw.rectangle([0, top, size, bottom], fill=rgb_bg)
+
+    # 保存图像
     filename = (
         f"{rgb_fg[0]}_{rgb_fg[1]}_{rgb_fg[2]}_{rgb_bg[0]}_{rgb_bg[1]}_{rgb_bg[2]}.png"
     )
     os.makedirs(output_dir, exist_ok=True)
     image.save(os.path.join(output_dir, filename))
-    print(f"Saved image as {filename}")
+    print(f"image saved as {filename}")
 
 
-# Generate 720x720 images
-for i in range(1):
-    # rgb_fg, rgb_bg = fgbgcolor()
-    rgb_fg = (0, 255, 0)
-    rgb_bg = (0, 0, 255)
-    plot_pillow(rgb_fg, rgb_bg, 720, 16)  # Adjust n to change the number of squares
+# 生成720x720的图像
+for i in range(2):
+    rgb_fg, rgb_bg = fgbgcolor()
+    plot_pillow(rgb_fg, rgb_bg, 720, 20, 4)
+# size = 720指的是720x720的图像
+# n = 16指的是长或宽为16个小正方形
+# grid_line_factor = 4指的是网格线的宽度影响因子，值越大，网格线越细
+# 4和16或20左右是比较好的一种组合
